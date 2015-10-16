bfe723f87ad30055f8574efb8aa592cc563e705c
sQuark23 <sQuark23@gmail.com>
2015-10-16 20:32:09 +0300

    Modified resources.json file and added testGenerateJsonResource() method in ApplicationTest class

--- app/src/androidTest/java/com/raccoonapps/worksimple/ApplicationTest.java
+++ app/src/androidTest/java/com/raccoonapps/worksimple/ApplicationTest.java
@@ -8,6 +8,10 @@ import android.util.Log;
 import com.raccoonapps.worksimple.model.ApplicationPropertiesLoader;
 import com.raccoonapps.worksimple.model.Category;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.List;
 
 /**
@@ -26,6 +30,27 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         int buttonId = ApplicationPropertiesLoader.getLoader(getContext()).getButtonIdByName(ApplicationPropertiesLoader.BUTTONS.MORE);
         assertNotSame(0, buttonId);
         Log.d("JUNIT", "Button id: " + buttonId);
+    }
+
+    @FlakyTest
+    public void testGenerateJsonResource() throws JSONException {
+        String categoryTitle = "shoes";
+        String categoryIcon = categoryTitle + "_ic.png";
+
+        JSONObject category = new JSONObject();
+        category.put("category_title", categoryTitle);
+        category.put("category_icon", categoryIcon);
+
+        JSONArray accessories = new JSONArray();
+        for (int i = 0; i < 11; i++) {
+            JSONObject accessory = new JSONObject();
+            accessory.put("file_name", categoryTitle + (i + 1) + ".png");
+            accessory.put("coordinates", new JSONArray());
+            accessories.put(accessory);
+        }
+
+        category.put("category_resources", accessories);
+        Log.d("JSON", category.toString());
     }
 
 }
